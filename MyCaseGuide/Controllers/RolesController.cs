13f3fe37583bda84f0c246b301e44e6be884bc79
Musa Sule <musagadabs1@yahoo.com>
2019-01-23 11:26:28 +0100

    User Role Implemented

---
+++ MyCaseGuide/Controllers/RolesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MyCaseGuide.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCaseGuide.Controllers
+{
+    public class RolesController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+        // GET: Roles
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!IsAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var roles = _context.Roles.ToList();
+            return View();
+        }
+        private bool IsAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                //ApplicationDbContext context = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+
+                var getRole = userManager.GetRoles(user.GetUserId());
+                if (getRole[0].ToString() == "Administrator")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyCaseGuide/Models/LegalGuideUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MyCaseGuide.Models
+{
+    public static class LegalGuideUtility
+    {
+        //public static bool IsAdminUser()
+        //{
+        //    if (User.Identity.IsAuthenticated)
+        //    {
+        //        var user = User.Identity;
+        //        ApplicationDbContext context = new ApplicationDbContext();
+        //        var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+        //        var getRole = userManager.GetRoles(user.GetUserId());
+        //        if (getRole[0].ToString() == "Administrator")
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return false;
+        //    //throw new NotImplementedException();
+        //}
+    }
+}
