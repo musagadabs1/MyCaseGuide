6ad92774aad067323ab1a8957de2b99bd9ebb196
Musa Sule <musagadabs1@yahoo.com>
2019-01-25 08:07:27 +0100

    Document Model Created

---
+++ MyCaseGuide/Models/Document.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyCaseGuide.Models
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        [Required]
+        [Display(Name ="Case to Link")]
+        public int MyCaseId { get; set; }
+        [Required]
+        [Display(Name ="Document Name")]
+        public string DocName { get; set; }
+        [Required]
+        [Display(Name ="Assigned Date")]
+        [DataType(DataType.Date)]
+        public DateTime AssignedDate { get; set; }
+        public string Tags { get; set; }
+        [Display(Name ="Doc Description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime DateModified { get; set; }
+
+        public MyCase MyCase { get; set; }
+    }
+}
