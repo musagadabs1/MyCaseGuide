4f93c5ca21e54fd19d4837663b25c5e8c6b383a8
Musa Sule <musagadabs1@yahoo.com>
2019-01-30 20:24:47 +0100

    Custom Login Added

---
+++ InmateTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InmateTest.MvcApplication" Language="C#" %>
---
+++ InmateTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InmateTest.Startup))]
+namespace InmateTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
